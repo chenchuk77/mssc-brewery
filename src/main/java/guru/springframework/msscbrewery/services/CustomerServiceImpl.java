46f735819757cc6a12a407d7f6d981d52c2864b9
chenchuk77 <chenchuk@gmail.com>
2019-08-27 11:49:47 +0300

    completing all CRUD operations

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,19 +1,39 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CustomerDTO getCustomerById(UUID customerId) {
-        //return null;
         return CustomerDTO.builder()
                 .id(UUID.randomUUID())
                 .name("Jon Kon")
                 .build();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customerDTO) {
+        return CustomerDTO.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public CustomerDTO updateCustomer(UUID customerId, CustomerDTO customerDTO) {
+        log.debug("updating customer " + customerId);
+        return null;
+    }
+
+    @Override
+    public CustomerDTO deleteCustomerById(UUID customerId) {
+        log.debug("deleting customer " + customerId);
+        return null;
     }
 }
